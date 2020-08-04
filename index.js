1f86f93ae3dd67d55aefdd12f6e13c85d65f5f8a
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2020-08-05 01:58:20 +0700

    add: korona

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 const wa = require('@open-wa/wa-automate');
 const moment = require('moment-timezone');
+const korona = require('./korona');
+
+const debug = async (text) => {
+	console.log(moment.tz('Asia/Jakarta').format() + text);
+};
 
 const start = async (client) => {
-	console.log(moment.tz('Asia/Jakarta').format() + ' => Bot stiker dimulai');
+	debug(' => The bot has started');
 	// force curr session
 	client.onStateChanged((state) => {
-		console.log('statechanged', state);
+		debug(' => 🛠 state changed - ' + state);
 		if (state === 'CONFLICT') client.forceRefocus();
 	});
 	// handling message
 	client.onMessage(async (message) => {
+		const stickerMsg = ' => 🖼  someone made a sticker - ' + message.from + ' - ' + message.sender.pushname + ' - ' + message.chat.name;
+		const generalMsg = ' => 📩 someone sent a message - ' + message.from + ' - ' + message.sender.pushname + ' - ' + message.chat.name;
+		const waitSticker = 'Tunggu sebentar stiker lagi dibuat ⏳';
+		const waitRequest = 'Tunggu sebentar data sedang di proses ⏳';
 		// image with text #sticker
 		if (message.type === 'image' && message.caption === '#sticker') {
-			console.log(moment.tz('Asia/Jakarta').format() + ' => Ada yang membuat stiker: ' + message.from + ' ( ' + message.sender.pushname + ' | ' + message.chat.name + ' )');
+			debug(stickerMsg);
+			client.sendText(message.from, waitSticker);
 			const mediaData = await wa.decryptMedia(message);
 			const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString('base64')}`;
 			client.sendImageAsSticker(message.from, imageBase64);
 		}
 		// quoted image with text #sticker
 		if (message.quotedMsg && message.quotedMsg.type === 'image' && message.body === '#sticker') {
-			console.log(moment.tz('Asia/Jakarta').format() + ' => Ada yang membuat stiker: ' + message.from + ' ( ' + message.sender.pushname + ' | ' + message.chat.name + ' )');
+			debug(stickerMsg);
+			client.sendText(message.from, waitSticker);
 			const mediaData = await wa.decryptMedia(message.quotedMsg);
 			const imageBase64 = `data:${message.quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
 			client.sendImageAsSticker(message.from, imageBase64);
 		}
 		// hello message
 		if (message.body === '#hi' || message.body === '#hai' || message.body === '#halo' || message.body === '#hello') {
-			client.sendText(message.from, 'Hai, untuk membuat stiker kirim gambar atau quote sebuah gambar dengan caption #sticker');
+			debug(generalMsg);
+			let botFeatureMsg = 'Hai 🙋🏻‍♂️, dibawah ini beberapa fitur yang bisa kalian gunakan\n\n';
+			botFeatureMsg += '#sticker => Membuat stiker dari gambar 🖼\n';
+			botFeatureMsg += '#korona => Data Korona Indonesia 🦠\n';
+			client.sendText(message.from, botFeatureMsg);
+		}
+		// korona
+		if (message.body === '#korona') {
+			debug(generalMsg);
+			client.sendText(message.from, waitRequest);
+			client.sendText(message.from, await korona());
 		}
 	});
 };
