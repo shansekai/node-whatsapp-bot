633f2dbd8885b9e081d1f6b5a9c2dcdff9df7bae
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2020-08-12 00:50:35 +0700

    update: big fix

--- index.js
+++ index.js
@@ -7,56 +7,65 @@ const debug = async (text) => {
   console.log(tz('Asia/Jakarta').format() + text);
 };
 
+const messageHandler = async (message, client) => {
+  // eslint-disable-next-line object-curly-newline
+  const { from, sender, caption, type, quotedMsg, mimetype, body } = await message;
+  const madeStickerMessage = ` => 🖼 someone made a sticker - ${from} - ${sender.pushname}`;
+  const incomingMessage = ` => 📩 someone sent a message - ${from} - ${sender.pushname}`;
+  const waitingForStickerMessage = 'Tunggu sebentar stiker lagi dibuat ⏳';
+  const waitingForRequestsMessage = 'Tunggu sebentar data sedang di proses ⏳';
+  const captionText = caption ? caption.toLowerCase() : '';
+  const bodyText = body ? body.toLowerCase() : '';
+
+  // image caption contain #sticker or #stiker
+  if (captionText === '#sticker' || captionText === '#stiker') {
+    // general message
+    if (type === 'image') {
+      debug(madeStickerMessage);
+      client.sendText(from, waitingForStickerMessage);
+      const mediaData = await decryptMedia(message);
+      const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`;
+      await client.sendImageAsSticker(from, imageBase64);
+    }
+    // quoted message
+    if (quotedMsg.type === 'image') {
+      debug(madeStickerMessage);
+      client.sendText(from, waitingForStickerMessage);
+      const mediaData = await decryptMedia(quotedMsg);
+      const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
+      await client.sendImageAsSticker(from, imageBase64);
+    }
+  }
+  // info message
+  if (bodyText === '#hai' || bodyText === '#halo') {
+    debug(incomingMessage);
+    let botFeatureMsg = 'Hai 🙋🏻‍♂️, dibawah ini beberapa fitur yang bisa kalian gunakan\n\n';
+    botFeatureMsg += '#sticker => Membuat stiker dari gambar 🖼\n';
+    botFeatureMsg += '#korona => Data Korona Indonesia 🦠\n';
+    await client.sendText(from, botFeatureMsg);
+  }
+  // korona
+  if (bodyText === '#korona') {
+    debug(incomingMessage);
+    await client.sendText(from, waitingForRequestsMessage);
+    await client.sendText(from, await korona());
+  }
+};
+
 const start = async (client) => {
   debug(' => The bot has started');
-  // force curr session
+  // force current session
   client.onStateChanged((state) => {
     debug(` => 🛠 state changed - ${state}`);
     if (state === 'CONFLICT') client.forceRefocus();
   });
   // handling message
   client.onMessage(async (message) => {
-    const stickerMsg = ` => 🖼 someone made a sticker - ${message.from} - ${message.sender.pushname} - ${message.chat.name}`;
-    const generalMsg = ` => 📩 someone sent a message - ${message.from} - ${message.sender.pushname} - ${message.chat.name}`;
-    const waitSticker = 'Tunggu sebentar stiker lagi dibuat ⏳';
-    const waitRequest = 'Tunggu sebentar data sedang di proses ⏳';
-    // image with text #sticker
-    if (message.type === 'image' && message.caption === '#sticker') {
-      debug(stickerMsg);
-      client.sendText(message.from, waitSticker);
-      const mediaData = await decryptMedia(message);
-      const imageBase64 = `data:${message.mimetype};base64,${mediaData.toString('base64')}`;
-      await client.sendImageAsSticker(message.from, imageBase64);
-    }
-    // quoted image with text #sticker
-    if (message.quotedMsg && message.quotedMsg.type === 'image' && message.body === '#sticker') {
-      debug(stickerMsg);
-      client.sendText(message.from, waitSticker);
-      const mediaData = await decryptMedia(message.quotedMsg);
-      const imageBase64 = `data:${message.quotedMsg.mimetype};base64,${mediaData.toString('base64')}`;
-      await client.sendImageAsSticker(message.from, imageBase64);
-    }
-    // hello message
-    if (message.body === '#hi' || message.body === '#hai' || message.body === '#halo' || message.body === '#hello') {
-      debug(generalMsg);
-      let botFeatureMsg = 'Hai 🙋🏻‍♂️, dibawah ini beberapa fitur yang bisa kalian gunakan\n\n';
-      botFeatureMsg += '#sticker => Membuat stiker dari gambar 🖼\n';
-      botFeatureMsg += '#korona => Data Korona Indonesia 🦠\n';
-      await client.sendText(message.from, botFeatureMsg);
-    }
-    // korona
-    if (message.body === '#korona') {
-      debug(generalMsg);
-      await client.sendText(message.from, waitRequest);
-      await client.sendText(message.from, await korona());
-    }
+    messageHandler(message, client);
   });
 };
 
 // start wa client
-create({
-  autoRefresh: false,
-  qrRefreshS: 15,
-})
+create()
   .then(async (client) => start(client))
   .catch((error) => console.log(error));
