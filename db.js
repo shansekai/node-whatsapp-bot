30099d3e767a47b12c31bb50d5f86553cf9af3c1
Mukhlis Akbarrudin <mukhlis3389@gmail.com>
2020-08-20 16:46:22 +0700

    refactoring

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true, useUnifiedTopology: true });
+dotenv.config();
+
+const url = process.env.MONGO_URL;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
